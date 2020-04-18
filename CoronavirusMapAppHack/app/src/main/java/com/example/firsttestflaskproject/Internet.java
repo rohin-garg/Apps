43cf4344437be7ff90e44885ce76fb60850ff53b
unknown <rohingarg123@gmail.com>
2020-04-17 22:26:23 -0700

    Initial coronavirus app for hackathon.

---
+++ CoronavirusMapAppHack/app/src/main/java/com/example/firsttestflaskproject/Internet.java
@@ -0,0 +1,27 @@
+package com.example.firsttestflaskproject;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
+public class Internet {
+    private Context context;
+
+    public Internet(Context context) {
+        this.context = context;
+    }
+
+    public Boolean Check() {
+        ConnectivityManager cn = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo nf = cn.getActiveNetworkInfo();
+        if (nf != null && nf.isConnected() == true) {
+            return true;
+        } else {
+            Toast.makeText(context, "No internet connection.!",
+                    Toast.LENGTH_LONG).show();
+            return false;
+        }
+    }
+}
